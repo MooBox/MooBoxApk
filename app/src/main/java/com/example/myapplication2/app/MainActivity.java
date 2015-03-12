fe6a81c80cbd1edd683594e4a7615d8d42212bb1
Darksidious <master.darksidious@gmail.com>
2015-03-12 13:41:43 +0100

    Improvment

--- app/src/main/java/com/example/myapplication2/app/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/app/MainActivity.java
@@ -1,23 +1,29 @@
 package com.example.myapplication2.app;
 
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
 
 public class MainActivity extends ActionBarActivity {
-    
-    ImageButton imageButton;
+
+    private ImageButton imageButton;
+    private ImageButton greenButton;
+    private ImageButton redButton;
     private static Socket socket;
+    private String host = "192.168.0.10";
+    private int port = 8091;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,49 +35,21 @@ public class MainActivity extends ActionBarActivity {
         this.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                try
-                {
-                    String host = "192.168.0.10";
-                    int port = 8091;
-                    InetAddress address = InetAddress.getByName(host);
-                     socket = new Socket(address, port);
-
-                    //Send the message to the server
-                    OutputStream os = socket.getOutputStream();
-                    OutputStreamWriter osw = new OutputStreamWriter(os);
-                    BufferedWriter bw = new BufferedWriter(osw);
-
-                    String number = "2";
-
-                    String sendMessage = number + "\n";
-                    bw.write(sendMessage);
-                    bw.flush();
-                    System.out.println("Message sent to the server : "+sendMessage);
-
-                    //Get the return message from the server
-                    InputStream is = socket.getInputStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
-                    String message = br.readLine();
-                    System.out.println("Message received from the server : " +message);
-                }
-                catch (Exception exception)
-                {
-                    exception.printStackTrace();
-                }
-                finally
-                {
-                    //Closing the socket
-                    try
-                    {
-                        socket.close();
-                    }
-                    catch(Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-                }
+                sendCommand("1&2");
+            }
+        });
+        this.greenButton = (ImageButton) this.findViewById(R.id.greenButton);
+        this.greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommand("1");
+            }
+        });
+        this.redButton = (ImageButton) this.findViewById(R.id.redButton);
+        this.redButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCommand("2");
             }
         });
 
@@ -98,5 +76,41 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendCommand(String param)
+    {
+        try {
+            InetAddress address = InetAddress.getByName(host);
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(address, port);
+            socket = new Socket();
+            socket.connect(inetSocketAddress, 4000);
+
+            //Send the message to the server
+            OutputStream os = socket.getOutputStream();
+            OutputStreamWriter osw = new OutputStreamWriter(os);
+            BufferedWriter bw = new BufferedWriter(osw);
+
+
+            String sendMessage = param;
+            bw.write(sendMessage);
+            bw.flush();
+            System.out.println("Message sent to the server : " + sendMessage);
+
+        }
+        catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        finally {
+            //Closing the socket
+            try {
+                if(socket!=null && socket.isConnected()) {
+                    socket.close();
+                }
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
