191f509683726c668698ef60b7372fba8b89b7db
Darksidious <master.darksidious@gmail.com>
2015-03-23 14:37:22 +0100

    app running with preferences

--- app/src/main/java/com/moocorder/MainActivity.java
+++ app/src/main/java/com/moocorder/MainActivity.java
@@ -1,6 +1,5 @@
 package com.moocorder;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -28,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
     private static Socket socket;
     private String host = "192.168.0.10";
     private int port = 80;
-    private String username = "Anonymous";
+    private String mooer = "Anonymous";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,32 +35,30 @@ public class MainActivity extends ActionBarActivity {
         StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
         this.imageButton = (ImageButton) this.findViewById(R.id.mooButton);
         this.imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendCommand("1&2");
+                sendCommand("1AND2", PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("mooer", mooer));
             }
         });
         this.greenButton = (ImageButton) this.findViewById(R.id.greenButton);
         this.greenButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendCommand("1");
+                sendCommand("1", PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("mooer", mooer));
             }
         });
         this.redButton = (ImageButton) this.findViewById(R.id.redButton);
         this.redButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendCommand("2");
+                sendCommand("2", PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("mooer", mooer));
             }
         });
-        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        username = defaultSharedPreferences.getString("username", username);
 
-        host = defaultSharedPreferences.getString("url", host);
-        port = Integer.valueOf(defaultSharedPreferences.getString("port", port + ""));
     }
 
 
@@ -86,15 +83,15 @@ public class MainActivity extends ActionBarActivity {
         }
     }
 
-    public void sendCommand(final String param) {
+    public void sendCommand(final String param, final String mooer) {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
                 Looper.prepare();
                 try {
 
-                    InetAddress address = InetAddress.getByName(host);
-                    InetSocketAddress inetSocketAddress = new InetSocketAddress(address, port);
+                    InetAddress address = InetAddress.getByName(PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("host", host));
+                    InetSocketAddress inetSocketAddress = new InetSocketAddress(address, Integer.valueOf(PreferenceManager.getDefaultSharedPreferences(getBaseContext()).getString("port", port + "")));
 
                     socket = new Socket();
                     socket.connect(inetSocketAddress, 4000);
@@ -105,17 +102,14 @@ public class MainActivity extends ActionBarActivity {
                     BufferedWriter bw = new BufferedWriter(osw);
 
 
-                    String sendMessage = param;
+                    String sendMessage = "moobox.function=" + param + "&moobox.mooer=" + mooer;
                     bw.write(sendMessage);
-                    bw.newLine();
                     bw.flush();
                     BufferedReader in =
                             new BufferedReader(
                                     new InputStreamReader(socket.getInputStream()));
-                    String response = in.readLine();
-
-                    System.out.println("Message sent to the server : " + sendMessage + " Response : "+response);
-                    Toast toast = Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG);
+                    System.out.println("Message sent to the server : " + sendMessage);
+                    Toast toast = Toast.makeText(getApplicationContext(), "Message sent to the server : " + sendMessage, Toast.LENGTH_LONG);
                     toast.show();
 
 
